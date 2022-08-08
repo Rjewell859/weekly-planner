71eac332144d6b79c8a10848910b3250b733440a
Headwall Forest <headwallforest27@gmail.com>
2022-08-08 15:59:19 -0400

    Updating with some javascript

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+var currentDayEl = $("#currentDay");
+var hourEl = $('.hour');
+var descriptionEl = $('.description');
+var saveButtonEl = $('.saveButton');
+var timeBlockEl = $('.timeBlock');
+var button1El = $('#button1');
+var desc1El = $('#desc1');
+
 var today = moment();
-var dayWeek = today.format("dddd, MMM Do")
+var dayWeek = today.format("dddd, MMM Do");
 
 var currentTime = today.format("hhA");
-$("#currentDay").text(dayWeek);
-$('.description').text(currentTime);
+currentDayEl.text(dayWeek);
+descriptionEl.text(currentTime);
 
+button1El.on('click', function(){
+    console.log(desc1El.value);
+    var newText = localStorage.getItem('savedText') + desc1El.text();
+    localStorage.setItem('savedText', newText);
+    console.log(localStorage.getItem('savedText'));
+    desc1El.text(newText);
+    
+})
