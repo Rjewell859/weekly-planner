88f54b75a7f839f649e18fe5b778b38b275cf81e
Headwall Forest <headwallforest27@gmail.com>
2022-08-08 17:51:57 -0400

    Further enhanced site appearance and function. Color changing background needs work.

--- script.js
+++ script.js
@@ -21,49 +21,77 @@ var button8El = $('#button8');
 var desc8El = $('#desc8');
 var button9El = $('#button9');
 var desc9El = $('#desc9');
+var savedEl = $('#saved');
+var localEl = $('#local');
+var hour1El = $('#hour1');
+var hour2El = $('#hour2');
+var hour3El = $('#hour3');
+var hour4El = $('#hour4');
+var hour5El = $('#hour5');
+var hour6El = $('#hour6');
+var hour7El = $('#hour7');
+var hour8El = $('#hour8');
+var hour9El = $('#hour9');
+
 
 var today = moment();
 var dayWeek = today.format("dddd, MMM Do");
-
 var currentTime = today.format("hhA");
 currentDayEl.text(dayWeek);
-descriptionEl.text(currentTime);
 
+var savedDisplay = function () {
+
+    savedEl.text("Event saved in ");
+    localEl.text("local storage ✔");
+    savedEl.append(localEl);
+    setTimeout(function () {
+        savedEl.text('')
+    }, 2000);
+}
 button1El.on('click', function () {
     var newText = desc1El.val();
     localStorage.setItem('savedText1', newText);
+    savedDisplay();
 })
 button2El.on('click', function () {
     var newText = desc2El.val();
     localStorage.setItem('savedText2', newText);
+    savedDisplay();
 })
 button3El.on('click', function () {
     var newText = desc3El.val();
     localStorage.setItem('savedText3', newText);
+    savedDisplay();
 })
 button4El.on('click', function () {
     var newText = desc4El.val();
     localStorage.setItem('savedText4', newText);
+    savedDisplay();
 })
 button5El.on('click', function () {
     var newText = desc5El.val();
     localStorage.setItem('savedText5', newText);
+    savedDisplay();
 })
 button6El.on('click', function () {
     var newText = desc6El.val();
     localStorage.setItem('savedText6', newText);
+    savedDisplay();
 })
 button7El.on('click', function () {
     var newText = desc7El.val();
     localStorage.setItem('savedText7', newText);
+    savedDisplay();
 })
 button8El.on('click', function () {
     var newText = desc8El.val();
     localStorage.setItem('savedText8', newText);
+    savedDisplay();
 })
 button9El.on('click', function () {
     var newText = desc9El.val();
     localStorage.setItem('savedText9', newText);
+    savedDisplay();
 })
 desc1El.text(localStorage.getItem('savedText1'));
 desc2El.text(localStorage.getItem('savedText2'));
